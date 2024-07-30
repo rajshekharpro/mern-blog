8652c34bd10e727178c4509a32967321b0846a21
rajshekharpro <raj@shekharpro.com>
2024-07-30 14:57:28 +0530

    sign-up page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,19 @@
 
 
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
 
 export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    flowbite.plugin(),
   ],
   
 };
