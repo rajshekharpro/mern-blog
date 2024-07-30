20c9e50644d1e7ca8d03a4534a303094fbcf5613
rajshekharpro <raj@shekharpro.com>
2024-07-30 11:48:49 +0530

    error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
-export const signup = async (req, res) => {
+import { errorHandler } from '../utils/error.js';
+  export const signup = async (req, res, next) => {
 
   const { username, email, password } = req.body;
 
@@ -13,7 +13,7 @@ export const signup = async (req, res) => {
     email === '' ||
     password === ''
   ) {
-    return res.status(400).json({ message: 'All fields are required' });
+    next(errorHandler(400, 'All fields are required'));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -28,6 +28,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json('Signup successful');
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
