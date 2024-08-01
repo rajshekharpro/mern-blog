3717d196bc390816842f34536c5f3356fa561292
rajshekharpro <raj@shekharpro.com>
2024-08-01 18:08:53 +0530

    complete dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
-import React from 'react';
-
-function Dashboard() {
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
     </div>
   );
 }
-
-export default Dashboard;
